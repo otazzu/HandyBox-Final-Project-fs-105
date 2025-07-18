997e7cf492d3fd0d464f09235d8008f8d96a9f17
Pedro Vélez <pedrolo.pv@gmail.com>
2025-07-18 18:59:11 +0000

    fixed error

--- src/wsgi.py
+++ src/wsgi.py
@@ -1,7 +1,11 @@
 # This file was created to run the application on heroku using gunicorn.
 # Read more about it here: https://devcenter.heroku.com/articles/python-gunicorn
 
-from app import app as application
+from app import app as application, socketio
+import os
+
 
 if __name__ == "__main__":
-    application.run()
+    #application.run()
+    PORT = int(os.environ.get('PORT', 3001))
+    socketio.run(application, host='0.0.0.0', port=PORT, debug=True)
