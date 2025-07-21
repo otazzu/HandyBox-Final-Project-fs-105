2f283e9e32e7b316d2a971ca3481c91d0776921e
Pedro Vélez <pedrolo.pv@gmail.com>
2025-07-21 17:54:48 +0000

    fixed socket io

--- src/wsgi.py
+++ src/wsgi.py
@@ -1,15 +1,15 @@
 # This file was created to run the application on heroku using gunicorn.
 # Read more about it here: https://devcenter.heroku.com/articles/python-gunicorn
 
-from app import app as application
+from app import app
 import os
-from extensions import socketio
+from extensions import socketio as application
 
 
-socketio.init_app(application)
+application.init_app(app)
 
 if __name__ == "__main__":
     import eventlet
     import eventlet.wsgi
     PORT = int(os.environ.get('PORT', 3001))
-    socketio.run(application, host='0.0.0.0', port=PORT, debug=True)
+    application.run(app, host='0.0.0.0', port=PORT, debug=True)
